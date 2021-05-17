47b53a55ffd534f65e511b868e989672b5bdcef4
gerardgb7 <g.grauges3@gmail.com>
2021-05-17 18:43:53 +0200

    Groundtruth

---
+++ code/ground_truth.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 17 16:35:35 2021
+
+@author: Hp
+"""
+#Matrius ground truth
+
+#image2
+image2_gt = [['B' 'N' 'I' 'L' 'A' 'S' 'A' 'S' 'N' 'H' 'O' 'B' 'P' 'C' 'O' 'E' 'L' 'O' 'V' 'E']
+ ['E' 'D' 'O' 'M' 'I' 'N' 'O' 'I' 'I' 'A' 'L' 'B' 'E' 'I' 'O' 'R' 'D' 'A' 'L' 'A']
+ ['R' 'D' 'E' 'L' 'A' 'S' 'T' 'G' 'Z' 'O' 'M' 'L' 'O' 'R' 'A' 'I' 'S' 'E' 'U' 'P']
+ ['I' 'A' 'I' 'U' 'E' 'E' 'O' 'E' 'N' 'L' 'O' 'I' 'E' 'P' 'R' 'N' 'C' 'T' 'A' 'R']
+ ['L' 'N' 'G' 'O' 'L' 'G' 'D' 'D' 'A' 'S' 'R' 'N' 'A' 'R' 'E' 'T' 'O' 'D' 'A' 'E']
+ ['I' 'I' 'D' 'O' 'R' 'U' 'A' 'R' 'O' 'O' 'E' 'S' 'A' 'F' 'O' 'R' 'A' 'L' 'P' 'C']
+ ['O' 'S' 'B' 'E' 'R' 'E' 'E' 'L' 'S' 'V' 'T' 'B' 'O' 'R' 'I' 'C' 'L' 'N' 'A' 'I']
+ ['C' 'A' 'C' 'E' 'S' 'N' 'T' 'E' 'F' 'I' 'O' 'G' 'A' 'D' 'E' 'I' 'O' 'E' 'L' 'S']
+ ['A' 'K' 'A' 'O' 'E' 'O' 'C' 'S' 'L' 'F' 'N' 'L' 'A' 'D' 'T' 'L' 'I' 'P' 'A' 'A']
+ ['R' 'I' 'N' 'G' 'R' 'C' 'R' 'L' 'A' 'I' 'P' 'D' 'N' 'N' 'I' 'N' 'M' 'O' 'D' 'R']
+ ['A' 'S' 'T' 'S' 'A' 'I' 'A' 'D' 'R' 'N' 'I' 'E' 'U' 'B' 'O' 'S' 'O' 'T' 'A' 'L']
+ ['C' 'T' 'O' 'U' 'L' 'R' 'E' 'G' 'E' 'O' 'V' 'P' 'A' 'P' 'A' 'Q' 'U' 'E' 'T' 'E']
+ ['O' 'E' 'R' 'M' 'R' 'A' 'P' 'U' 'A' 'N' 'O' 'B' 'O' 'T' 'E' 'L' 'L' 'A' 'N' 'I']
+ ['L' 'S' 'A' 'N' 'U' 'D' 'I' 'A' 'M' 'E' 'T' 'R' 'O' 'I' 'C' 'R' 'E' 'T' 'U' 'F']
+ ['A' 'T' 'L' 'T' 'B' 'V' 'A' 'R' 'A' 'D' 'A' 'A' 'L' 'L' 'I' 'N' 'A' 'L' 'J' 'O']]
+
+#image3
+
+image3_gt = [['R' 'A' 'L' 'I' 'N' 'E' 'A' 'R' 'C' 'U' 'O' 'T' 'A' 'O' 'S' 'O' 'P' 'A' 'R' 'O']
+ ['E' 'E' 'A' 'C' 'O' 'R' 'T' 'A' 'F' 'R' 'I' 'O' 'A' 'J' 'E' 'O' 'D' 'I' 'A' 'C']
+ ['G' 'R' 'N' 'A' 'T' 'S' 'E' 'L' 'L' 'A' 'B' 'D' 'B' 'T' 'O' 'E' 'T' 'I' 'U' 'T']
+ ['A' 'C' 'A' 'A' 'P' 'D' 'A' 'N' 'E' 'Y' 'A' 'C' 'N' 'A' 'I' 'N' 'O' 'L' 'A' 'V']
+ ['Z' 'O' 'C' 'T' 'A' 'L' 'A' 'P' 'E' 'G' 'O' 'E' 'A' 'N' 'B' 'V' 'J' 'P' 'A' 'R']
+ ['O' 'I' 'A' 'N' 'S' 'G' 'E' 'R' 'N' 'O' 'N' 'A' 'S' 'R' 'E' 'U' 'O' 'O' 'A' 'S']
+ ['R' 'D' 'B' 'E' 'E' 'T' 'M' 'E' 'S' 'I' 'O' 'A' 'I' 'N' 'O' 'N' 'I' 'T' 'L' 'S']
+ ['A' 'E' 'A' 'I' 'A' 'S' 'R' 'O' 'M' 'E' 'L' 'T' 'G' 'M' 'P' 'F' 'N' 'N' 'I' 'I']
+ ['D' 'M' 'R' 'T' 'R' 'E' 'S' 'E' 'R' 'I' 'N' 'A' 'A' 'E' 'A' 'E' 'A' 'D' 'O' 'D']
+ ['O' 'F' 'E' 'E' 'M' 'A' 'L' 'U' 'V' 'T' 'N' 'A' 'N' 'C' 'I' 'N' 'E' 'N' 'O' 'E']
+ ['O' 'N' 'A' 'I' 'N' 'E' 'K' 'A' 'O' 'Z' 'E' 'D' 'D' 'B' 'A' 'C' 'U' 'T' 'A' 'M']
+ ['M' 'A' 'N' 'A' 'G' 'E' 'R' 'N' 'A' 'M' 'U' 'C' 'M' 'L' 'A' 'S' 'A' 'S' 'R' 'O']
+ ['A' 'A' 'R' 'E' 'C' 'A' 'B' 'A' 'U' 'L' 'Q' 'A' 'I' 'R' 'E' 'R' 'E' 'C' 'T' 'N']
+ ['R' 'S' 'A' 'N' 'D' 'E' 'Z' 'R' 'A' 'P' 'I' 'F' 'A' 'N' 'E' 'C' 'A' 'D' 'O' 'I']
+ ['T' 'R' 'U' 'J' 'A' 'L' 'O' 'R' 'P' 'U' 'L' 'S' 'O' 'S' 'O' 'J' 'U' 'L' 'P' 'O']]
+
+
+#image4
+
+image4_gt = [['O' 'I' 'R' 'A' 'V' 'I' 'U' 'Q' 'S' 'E' 'M' 'A' 'C' 'U' 'L' 'A' 'R' 'G' 'A' 'R']
+ ['A' 'R' 'U' 'T' 'U' 'S' 'A' 'R' 'E' 'N' 'I' 'L' 'O' 'M' 'E' 'B' 'A' 'D' 'M' 'N']
+ ['P' 'A' 'T' 'O' 'A' 'A' 'C' 'I' 'P' 'A' 'C' 'I' 'P' 'N' 'E' 'J' 'E' 'D' 'U' 'O']
+ ['U' 'R' 'O' 'N' 'C' 'R' 'R' 'M' 'C' 'O' 'P' 'E' 'V' 'R' 'A' 'F' 'R' 'O' 'L' 'D']
+ ['R' 'E' 'R' 'I' 'H' 'R' 'I' 'A' 'N' 'U' 'N' 'O' 'L' 'A' 'E' 'C' 'B' 'S' 'T' 'I']
+ ['E' 'E' 'E' 'A' 'E' 'O' 'A' 'F' 'E' 'C' 'C' 'U' 'L' 'C' 'N' 'A' 'E' 'I' 'I' 'P']
+ ['P' 'S' 'C' 'R' 'P' 'N' 'O' 'Z' 'A' 'T' 'M' 'A' 'C' 'I' 'L' 'I' 'L' 'S' 'P' 'T']
+ ['E' 'O' 'I' 'E' 'A' 'R' 'O' 'J' 'E' 'I' 'R' 'I' 'A' 'E' 'S' 'A' 'D' 'C' 'L' 'I']
+ ['R' 'P' 'P' 'V' 'T' 'O' 'A' 'L' 'N' 'I' 'O' 'O' 'U' 'I' 'I' 'E' 'I' 'O' 'E' 'C']
+ ['I' 'T' 'A' 'O' 'I' 'R' 'I' 'A' 'O' 'N' 'P' 'Z' 'S' 'N' 'N' 'L' 'M' 'N' 'S' 'O']
+ ['T' 'R' 'L' 'C' 'C' 'I' 'R' 'C' 'O' 'I' 'R' 'S' 'H' 'E' 'A' 'A' 'I' 'I' 'E' 'O']
+ ['O' 'A' 'O' 'A' 'O' 'I' 'D' 'O' 'R' 'A' 'V' 'A' 'E' 'C' 'R' 'R' 'Z' 'B' 'A' 'V']
+ ['G' 'Z' 'N' 'R' 'A' 'G' 'U' 'F' 'Z' 'E' 'L' 'B' 'A' 'D' 'M' 'I' 'R' 'A' 'R' 'R']
+ ['A' 'A' 'O' 'D' 'A' 'I' 'P' 'O' 'C' 'A' 'H' 'Y' 'O' 'E' 'N' 'E' 'G' 'E' 'G' 'A']
+ ['F' 'R' 'M' 'O' 'R' 'E' 'C' 'U' 'R' 'C' 'O' 'L' 'O' 'R' 'E' 'N' 'R' 'O' 'H' 'P']]
+
+#image6
+
+image6_gt = [['R' 'A' 'M' 'U' 'F' 'L' 'A' 'L' 'E' 'V' 'A' 'N' 'T' 'A' 'R' 'A' 'O' 'Z' 'H' 'M']
+ ['E' 'T' 'R' 'O' 'N' 'A' 'P' 'A' 'L' 'O' 'S' 'M' 'O' 'D' 'O' 'R' 'R' 'A' 'E' 'O']
+ ['G' 'U' 'U' 'A' 'S' 'N' 'E' 'R' 'E' 'N' 'A' 'G' 'I' 'D' 'R' 'A' 'C' 'N' 'M' 'D']
+ ['O' 'N' 'G' 'D' 'R' 'T' 'N' 'B' 'T' 'R' 'P' 'N' 'N' 'O' 'R' 'O' 'R' 'E' 'A' 'A']
+ ['C' 'U' 'O' 'A' 'O' 'E' 'A' 'E' 'C' 'I' 'O' 'O' 'R' 'E' 'M' 'O' 'C' 'T' 'T' 'C']
+ ['I' 'M' 'S' 'M' 'D' 'O' 'S' 'C' 'A' 'A' 'I' 'I' 'P' 'E' 'L' 'S' 'A' 'S' 'O' 'I']
+ ['G' 'E' 'O' 'I' 'I' 'J' 'D' 'D' 'E' 'S' 'U' 'U' 'R' 'L' 'I' 'P' 'I' 'C' 'I' 'H']
+ ['A' 'R' 'R' 'A' 'T' 'O' 'O' 'A' 'N' 'R' 'T' 'S' 'A' 'A' 'R' 'T' 'U' 'R' 'O' 'C']
+ ['L' 'A' 'E' 'T' 'R' 'R' 'L' 'E' 'N' 'I' 'A' 'R' 'A' 'E' 'S' 'L' 'R' 'R' 'I' 'A']
+ ['A' 'L' 'T' 'M' 'U' 'E' 'S' 'A' 'V' 'I' 'T' 'L' 'M' 'N' 'E' 'I' 'A' 'S' 'C' 'L']
+ ['N' 'R' 'N' 'E' 'S' 'I' 'F' 'C' 'C' 'P' 'M' 'I' 'G' 'P' 'T' 'R' 'M' 'T' 'I' 'A']
+ ['I' 'A' 'E' 'N' 'D' 'O' 'B' 'L' 'I' 'S' 'A' 'U' 'D' 'E' 'I' 'E' 'I' 'E' 'F' 'H']
+ ['M' 'C' 'V' 'H' 'A' 'M' 'B' 'R' 'E' 'R' 'I' 'S' 'L' 'O' 'B' 'V' 'R' 'N' 'O' 'C']
+ ['A' 'O' 'T' 'I' 'F' 'A' 'R' 'G' 'N' 'J' 'I' 'R' 'O' 'I' 'O' 'R' 'R' 'E' 'B' 'U']
+ ['Z' 'T' 'U' 'R' 'N' 'O' 'V' 'I' 'L' 'L' 'O' 'O' 'B' 'O' 'R' 'G' 'A' 'N' 'O' 'D']]
