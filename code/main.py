7ea690b6606ce6ce59d03d1e147c63b6602880ae
Miquel <miquel.emt2@gmail.com>
2021-05-20 18:36:29 +0200

    new models

--- code/main.py
+++ code/main.py
@@ -2,52 +2,32 @@ import preprocessing
 import predictor
 import cv2
 import solver
+import ground_truth
 
-mode = "predict"
+mode = "preprocessing"
+
+gray = preprocessing.read_gray_image('../images/image4.jpeg')
+# threshold
+thresh = preprocessing.adaptative_threshold(gray)
+# inverse threhsold
+inv_thresh = preprocessing.inv_image(thresh)
+# remove extra info
+thresh, box = preprocessing.remove_extra_information(thresh, inv_thresh)
+# homography
+corners = preprocessing.get_corners(box)
+warped = preprocessing.homography(corners, thresh)
+# remove isolated pixels
+thresh, inv_thresh = preprocessing.remove_isolated_pixels(warped)
+
 
 if mode == "preprocessing":
-    #gray image
-    gray = preprocessing.read_gray_image('../images/image7.jpeg')
-    #threshold
-    thresh = preprocessing.adaptative_threshold(gray)
-    cv2.imshow("First thresh",thresh)
-    #inverse threhsold
-    inv_thresh = preprocessing.inv_image(thresh)
-    #remove extra info
-    thresh, box = preprocessing.remove_extra_information(thresh, inv_thresh)
-    cv2.imshow("Second thresh", thresh)
-    #homography
-    corners = preprocessing.get_corners(box)
-    warped = preprocessing.homography(corners, thresh)
-    cv2.imshow("Homography", warped)
-    #remove isolated pixels
-    thresh, inv_thresh = preprocessing.remove_isolated_pixels(warped)
-    cv2.imshow("Third thresh", thresh)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
     #draw characters contours
     preprocessing.draw_ctrs(inv_thresh, thresh)
 elif mode == "predict":
-    gray = preprocessing.read_gray_image('../images/image7.jpeg')
-    # threshold
-    thresh = preprocessing.adaptative_threshold(gray)
-    # inverse threhsold
-    inv_thresh = preprocessing.inv_image(thresh)
-    # remove extra info
-    thresh, box = preprocessing.remove_extra_information(thresh, inv_thresh)
-    # homography
-    corners = preprocessing.get_corners(box)
-    warped = preprocessing.homography(corners, thresh)
-    # remove isolated pixels
-    thresh, inv_thresh = preprocessing.remove_isolated_pixels(warped)
     result = predictor.predict_chars(inv_thresh,thresh)
     print(result)
+    acc = predictor.evaluate_model(result, ground_truth.image6_gt)
 elif mode == "generate_dataset":
-    gray = preprocessing.read_gray_image('../images/example2.png')
-    # threshold
-    thresh = preprocessing.adaptative_threshold(gray)
-    # inverse threhsold
-    inv_thresh = preprocessing.inv_image(thresh)
     #generate dataset
     predictor.generate_dataset(inv_thresh, thresh)
 
--- code/pretrain.py
+++ code/pretrain.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 import cv2
 import gt_dataset
+import ground_truth
 import matplotlib.pyplot as plt
 
 from keras.utils import to_categorical
@@ -19,14 +20,15 @@ sorted_files = sorted(files,key=lambda x: int(x.split(".")[0]))
 x = np.array([cv2.imread(path+file, 0) for file in sorted_files])
 x = x.reshape(x.shape[0], x.shape[1], x.shape[2],1)
 print("Shape of train data: ", x.shape)
-y = gt_dataset.gt_dataset01
+
+y = np.concatenate((ground_truth.example2_gt_num,ground_truth.image6_gt_num, ground_truth.image4_gt_num))
 y = to_categorical(y, num_classes = 26, dtype='int')
 print("Shape of train labels: ", y.shape)
 
 
 
 history = model.fit(x, y, epochs=10,  validation_data = (x,y), verbose=1)
-model.save("./model/model_print.h5")
+model.save("./model/model_print2.h5")
 
 N = 9
 pred = model.predict(x[:N])
